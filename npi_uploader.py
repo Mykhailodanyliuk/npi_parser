6ba997278a7856a9b30229eec2fd16970b2099fa
Mykhailo Danyliuk <mihailodaniliuk@gmail.com>
2023-03-14 14:39:22 +0200

    edited time.sleep

--- npi_uploader.py
+++ npi_uploader.py
@@ -106,6 +106,4 @@ if __name__ == '__main__':
         start_time = time.time()
         write_all_data_parser3()
         work_time = int(time.time() - start_time)
-        print(work_time)
-        print(14400 - work_time)
-        time.sleep(14400 - work_time)
+        time.sleep(abs(work_time % 14400 - 14400))
